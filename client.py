9514e3451827da6ed4828fced1457a19e24d47a6
Tyler Canepa <tmcanepa@ucsb.edu>
2024-11-15 16:17:19 -0800

    new functions

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ import threading
 import time
 import sys
 import heapq
+import queue
 
 
 load_dotenv()
@@ -20,11 +21,18 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 
 
 
-
+key_value_store = []
+leader_queue = queue.Queue()
+query_queue = queue.Queue()
 client_sockets = []
 running = True
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_id = 0
+curr_leader = 0
+ballot_num = [0,0]
+accept_num = [0,0]
+accept_val = None
+
 
 
 def handle_exit():
@@ -34,34 +42,80 @@ def handle_exit():
     sys.stdout.flush()
     sys.exit(0)
 
-
-def talk_to_server(message, client_id):
-    global active_peer_threads
-    global client_socket
-    if message.lower() == "exit":
-        handle_exit()
-    else:
-        client_socket.send(message.encode('utf-8'))
-        response = client_socket.recv(1024).decode('utf-8')
-        print(f"received {response}")
-        #handle_function() ie create,query, 
     
 
 def start_client(): #Allows other clients to talk to you, also connect to other clients from peer list
     global client_socket
     global client_id
     client_socket.connect(('127.0.0.1', port))
-    client_id = client_socket.getsockname()[1]
-    client_socket.send(f"{client_id}".encode('utf-8'))
+    port_num = client_socket.getsockname()[1]
+    client_socket.send(f"{client_id} {port_num}".encode('utf-8'))
     response = client_socket.recv(1024).decode('utf-8')
     if response != "Success":
         print("Failure to send server clientid")
-    while running:
-            message = input()
-            threading.Thread(target=talk_to_server, args=(message, client_id), daemon=True).start()
+    threading.Thread(target = handle_messages, ).start() #Needed for accept, ... (function name will change as more receive threads are understood)
 
 
+def propose(message, send_id1, send_id2): #starts the leader election 
+    global ballot_num
+    ballot_num[0] += 1
+    client_socket.send(f"{message} {ballot_num} {client_id} {send_id1} {send_id2}").encode('utf-8')
+    return
+
+def promise(bal, accept_num, accept_val, proposer):
+    client_socket.send(f"promise {bal} {accept_num} {accept_val} {proposer} {client_id}".encode('utf-8'))
+
+def accept(ballot_num, promiser):
+    client_socket.send(f"accept {ballot_num} {promiser} {client_id}".encode('utf-8'))
+
+def handle_messages(): #handles 
+    message = client_socket.recv(1024).decode('utf-8')
+    if message.startswith('prepare'):
+        _, bal, proposer, _, _  = message.split()
+        if bal >= ballot_num:
+            promise(bal, accept_num, accept_val, proposer)
+    elif message.startswith('promise'):
+        _, ballot_num, accept_num, accept_val, _, promiser = message.split()
+            accept(ballot_num, promiser)
+    if message.startswith('accept'):
+        _, b, promiser, acceptor = messaag
+        
+        
+
+    return
+
+
+def create_context(message):
+    if curr_leader == client_id:
+        _ , context_id = message.split()
+        key_value_store[context_id] = ""
+    else:
+        message = f"{message} {client_id} {curr_leader}"
+        send_to_leader(message)
+
+def query_context(message):
+    global leader_queue, query_queue
+    _ , context_id, query = message.split()
+    if curr_leader == client_id:
+        leader_queue.put((context_id, query))
+    else:
+        query_queue.put((context_id, query))
+        send_to_leader(message)
+
+def send_to_leader(message):
+    message = "{message} {client_id} {curr_leader}"
+    client_sockets[curr_leader - 1].send(message.encode('utf-8'))
     
+def choose_response(message):
+    return
+
+def view_context(message):
+    _, context_id = message.split()
+    print(key_value_store[context_id])
+
+def view_all_context():
+    print(key_value_store)
+
 
 if __name__ == "__main__":
     global port
@@ -69,5 +123,23 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         sys.exit(1)
     port = int(sys.argv[2])
-    # client_id = int(sys.argv[1])
+    client_id = int(sys.argv[1])
+    ballot_num[1] = client_id
     start_client()
+    if(client_id == 3):
+        propose("prepare", ballot_num, 1,2)
+    while running:
+        message = input()
+        if message.startswith('create'):
+            threading.Thread(target=create_context, args=(message,)).start()
+        if message.startswith('query'):
+            threading.Thread(target=query_context, args=(message,)).start()
+        if message.startswith('choose'):
+            threading.Thread(target=choose_response, args=(message,)).start()
+        if message.startswith('view '):
+            view_context(message)
+        if message == "viewall":
+            view_all_context()     
+        elif message == 'exit':
+            running = False
+            handle_exit()
--- server.py
+++ server.py
@@ -6,29 +6,49 @@ import time
 
 clients = []
 running = True
-id_dct = []
-def primary_handle_client(client_socket): #Everytime a client connects, send all other clients the updated peer list and handles input from clients
+id_dct = {}
+fail_dct = {}
+def primary_handle_client(client_socket): #Everytime a client connects, it has its own thread to communicate with the server
     global running
     while running:
         try:
             message = client_socket.recv(1024).decode('utf-8') 
             if not message:
                 break
-            threading.Thread(target=client_to_primary, args=(client_socket, message), daemon=True).start()
+            if message.startswith("prepare"):
+                parts = message.split()
+                send_id1 = parts[3]
+                send_id2 = parts[4]
+                print(f"Received prepare")
+                if(fail_dct[id_dct[send_id1]] == True): #send to both other clients
+                    threading.Thread(target=server_to_client, args=(clients[send_id1-1], message), daemon=True).start()
+                if(fail_dct[id_dct[send_id2]] == True):
+                    threading.Thread(target=server_to_client, args=(clients[send_id2-1], message), daemon=True).start()
+            elif message.startswith("promise"): #send back to proposer
+                _, _, _, _, proposer, promiser = message.split()
+                if fail_dct[id_dct[proposer]] == True:
+                    threading.Thread(target=server_to_client, args=(clients[proposer-1], message), daemon=True).start()
+            elif message.startswith("accept"): #send back to promiser
+                _, b, promiser, acceptor = message.split()
+                if fail_dct[id_dct[promiser]] == True:
+                    threading.Thread(target=server_to_client, args=(clients[promiser-1], message), daemon=True).start()
+            elif message.startswith("accepted"): #send back to promiser
+                _, b,  acceptor, acceptedor = message.split()
+                if fail_dct[id_dct[promiser]] == True:
+                    threading.Thread(target=server_to_client, args=(clients[promiser-1], message), daemon=True).start()
+            
+            elif message.startswith(create)
+                
         except ConnectionResetError:
             break
         except Exception as e:
             break
     client_socket.close() 
 
-def client_to_primary(client_socket, message): #handles client message in primary
+def server_to_client(client_socket, message): #handles send messages from server to clients
     try:
-        response = ""
         time.sleep(3)
-        parts = message.split()
-        print("The message received from client is", parts)
-        response = "Success"
-        client_socket.send(f"{response}".encode('utf-8'))  
+        client_socket.send(message.encode('utf-8'))  
     except Exception as e:
         pass
 
@@ -38,16 +58,7 @@ def primary_input_thread(): #primary input thread
     global running
     while running:
         command = input()
-        if command.lower() == "dictionary":
-            print("{", end ="")
-            i = 0
-            for key in sorted(ht):
-                if i != 0:
-                    print(", ", end ="")
-                print(f"({key}, {ht[key]})", end = "")
-                i += 1
-            print("}")
-        elif command.lower() == "exit":
+        if command.lower() == "exit":
             handle_exit()
 
 def handle_exit(): #handles exits
@@ -63,7 +74,7 @@ def handle_exit(): #handles exits
     sys.exit(0)
 
 
-def start_server(PORT): #Begines the input thread and accepts clients
+def start_server(PORT): #Begins the input thread and accepts clients
     global server_socket 
     global running
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
@@ -74,9 +85,10 @@ def start_server(PORT): #Begines the input thread and accepts clients
     while running:
         try:
             client_socket, _ = server_socket.accept()
-            clients.append(client_socket)  
-            client_id = client_socket.recv(1024).decode('utf-8')
-            id_dct.append(client_id)
+            clients.append(client_socket) #This only tracks order if clients are accepted as 1,2,3
+            client_id, port_num = client_socket.recv(1024).decode('utf-8').split()
+            id_dct[client_id] = port_num
+            fail_dct[port_num] = True
             client_socket.send("Success".encode('utf-8')) 
             client_handler = threading.Thread(target=primary_handle_client, args=(client_socket,), daemon=True) #handles incoming clinets
             client_handler.start()  
